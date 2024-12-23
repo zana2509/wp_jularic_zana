9d2184bf06ef02aeb652593e4d216dc255f6e6e4
zana2509 <zana.jularic@gmail.com>
2024-12-23 14:41:08 +0100

    dz i sa sata

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,23 @@
+/* var broj1 = prompt ("Unesi prvi broj: ");
+var broj2 = prompt ("Unesi drugi broj: ");
+
+if (broj1 > broj2)
+{
+    alert (broj1 + " je veći od broja " + broj2);
+}
+else if (broj1 < broj2)
+{
+  alert (broj2 + " je veći od broja " + broj1);
+}
+else
+{
+    alert ("Brojevi su jednaki");
+} */
+
+
+//Love calculator
+var ime1 = prompt ("Unesi svoje ime: ");
+var ime2 = prompt ("Unesi ime simpatije: ");
+
+var n = Math.floor (Math.random()*100)+1;
+alert ("Kompatibilni ste: " + n + "%");
