005ab95f6dca30368c3c70e5f0d13e571569612e
zana2509 <zana.jularic@gmail.com>
2025-01-23 18:57:41 +0100

    zadaca

---
+++ zadace/index.js
@@ -0,0 +1,9 @@
+// Pokreće prompt za unos broja
+const broj = prompt("Unesite broj:");
+
+// Provjerava je li broj veći od 10
+if (broj > 10) {
+  alert("Broj je veći od 10");
+} else {
+  alert("Broj nije veći od 10");
+}
